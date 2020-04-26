2041485543feb49cf2deaa5e283ee9263cffe728
R0ma-N <x6wert@gmail.com>
2020-04-26 23:17:37 +0300

    PW2

---
+++ PatternsPW2/PatternsPW2/Part_1/Interfaces/IModifyOrder.cs
@@ -0,0 +1,16 @@
+﻿
+namespace PatternsPW2.Part_1
+{
+    interface IModifyOrder 
+    {
+        public void Update(Order order)
+        {
+            //...
+        }
+
+        public void Delete(Order order)
+        {
+            //...
+        }
+    }
+}
---
+++ PatternsPW2/PatternsPW2/Part_2/Interfaces/IColor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatternsPW2.Part_2.Interfaces
+{
+    interface IColor
+    {
+        public void SetColor(Color color) {  ;}
+    }
+}
